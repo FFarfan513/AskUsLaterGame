68a59f1eefdaf8794a5b42a7c851766a23b9b51d
Will-Bahnmiller <will.bahnmiller@gmail.com>
2015-04-03 14:19:20 -0400

    4/3 Update
    added spikey enemy.

---
+++ Project/Assets/Scripts/EnemySticky.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySticky : MonoBehaviour {
+
+	EnemyController controller;
+	private bool canMove;
+	private Vector3 followPos;
+
+	void Start() {
+		controller = gameObject.GetComponent<EnemyController>();
+		canMove = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (canMove) {
+			followPos = controller.followMe.transform.position;
+			transform.position = controller.DumbSeek(transform.position, followPos);
+		}
+	}
+
+	void OnTriggerEnter2D( Collider2D other ) {
+		if (other.tag == controller.GetPlayerTag()) {
+			controller.Damage();
+			controller.KillMe();
+		}
+		else if (other.tag == "Wall"){
+			canMove = false;	// if collides into wall, 
+		}
+	}
+}
