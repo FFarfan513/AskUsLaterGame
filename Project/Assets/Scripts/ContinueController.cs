921e45fc6276364430dba9eec58891aa19bf4397
Will-Bahnmiller <will.bahnmiller@gmail.com>
2015-04-17 16:16:45 -0400

    4/17 Mini Push 2

---
+++ Project/Assets/Scripts/ContinueController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinueController : MonoBehaviour {
+
+	/*
+	void OnMouseDown() {
+		if (this.name == "Yes") {
+			Application.LoadLevel( PlayerPrefs.GetInt("level") );
+		}
+		else if (this.name == "No") {
+			PlayerPrefs.SetInt("level", 0);
+			Application.LoadLevel("Main");
+		}
+	}
+
+	void OnGUI() {
+		GUIStyle style = new GUIStyle ();
+		style.fontSize = (int)(Screen.height*0.05);
+		style.normal.textColor = Color.white;
+		GUI.Label( new Rect( (float)(Screen.width*0.495), (float)(Screen.height*0.025), 100, 20 ), , style );
+	}
+	*/
+}
--- Project/Assets/Scripts/LevelTransitionController.cs
+++ Project/Assets/Scripts/LevelTransitionController.cs
@@ -6,18 +6,29 @@ public class LevelTransitionController : MonoBehaviour {
 	private GameObject[] goals;
 	private readonly int maxHP = 8;
 	private static int HP, levelCounter;
+	private bool g1, g2;
 
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	
 	void Start() {
 		HP = maxHP;
 		levelCounter = 0;
 		goals = GameObject.FindGameObjectsWithTag("Goal");
 	}
 
-	void Update() {		
-		if (goals[0].GetComponent<GoalController>().goalReached &&
-		    goals[1].GetComponent<GoalController>().goalReached) {
-			levelCounter++;
-			loadNextLevel();
+	void Update() {
+		if (goals != null && goals[0] != null && goals[1] != null) {
+			
+			g1 = goals[0].GetComponent<GoalController>().goalReached;
+			g2 = goals[1].GetComponent<GoalController>().goalReached;
+			
+			if ( g1 && g2 ) {
+				goals = null;
+				levelCounter++;
+				loadNextLevel();
+			}
 		}
 	}
 
@@ -29,18 +40,25 @@ public class LevelTransitionController : MonoBehaviour {
 
 	public static void DecrementHP(){
 		HP--;
-		if (HP <= 0) {
+		if (HP <= 0) { // Gameover -> Continue Scene
 			Debug.Log ("death");
-			PlayerPrefs.SetInt("level", levelCounter);
 			Application.LoadLevel("Continue");
 		}
 	}
 	
 	void OnGUI() {
-		GUIStyle style = new GUIStyle ();
-		style.fontSize = (int)(Screen.height*0.05);
-		style.normal.textColor = Color.white;
-		GUI.Label( new Rect( (float)(Screen.width*0.495), (float)(Screen.height*0.025), 100, 20 ), HP.ToString(), style );
+		if (HP > 0) {
+			GUIStyle style = new GUIStyle ();
+			style.fontSize = (int)(Screen.height*0.05);
+			style.normal.textColor = Color.white;
+			GUI.Label( new Rect( (float)(Screen.width*0.495), (float)(Screen.height*0.025), 100, 20 ), HP.ToString(), style );
+		}
+		else {
+			GUIStyle style = new GUIStyle ();
+			style.fontSize = (int)(Screen.height*0.1);
+			style.normal.textColor = Color.white;
+			GUI.Label( new Rect( (float)(Screen.width*0.5), (float)(Screen.height*0.025), 100, 20 ), "Gameover", style );
+		}
 	}
 
 }
