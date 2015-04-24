2d270a7fbdfddee576d905d7e282e5c78bd3aaa2
curleyd <curleyd@knights.ucf.edu>
2015-04-24 15:37:04 -0400

    Added tutorial screen

--- Project/Assets/Scripts/TitleController.cs
+++ Project/Assets/Scripts/TitleController.cs
@@ -4,15 +4,22 @@ using System.Collections;
 public class TitleController : MonoBehaviour {
 	private LevelTransitionController main;
 	public AudioClip click;
-	private static bool loading;
+	private static bool loading, inTutorial;
 
 	private AudioSource source;
 	private float lowPitch = 0.9f, highPitch = 1.1f;
 
 	void Start() {
 		loading = false;
+		inTutorial = false;
 		main = Camera.main.GetComponent<LevelTransitionController>();
 		source = Camera.main.GetComponent<AudioSource>();
+	}
+
+	void Update() {
+		if (inTutorial && this.name == "Help") {
+			Tutorial();
+		}
 	}
 
 	void OnMouseDown() {
@@ -24,11 +31,31 @@ public class TitleController : MonoBehaviour {
 				loading = true;
 			}
 			if (this.name == "Help") {
-				print("Help!\n");
+				inTutorial = true;
+				Vector3 cam = new Vector3(-20,40,-10);
+				Camera.main.transform.position = cam;
 			}
 			if (this.name == "Credits") {
 				print("Made by me\n");
 			}
 		}
+	}
+
+	void Tutorial() {
+
+		Vector3 camTemp = Camera.main.transform.position;
+
+		if(Input.GetMouseButtonDown (0))
+			camTemp.x += 20;
+		if(Input.GetMouseButtonDown (1))
+			camTemp.x -= 20;
+		
+		if(camTemp.x > 101 || camTemp.x < -1) {
+			camTemp.x = 0;
+			camTemp.y = 0;
+			inTutorial = false;
+		}
+		
+		Camera.main.transform.position = camTemp;
 	}
 }
